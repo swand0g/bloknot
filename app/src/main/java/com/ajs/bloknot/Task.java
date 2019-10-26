17bd0709036392a2c97ab5b49e636bcabe8bd7e2
ajsv <alexanderjswanson@icloud.com>
2019-10-26 18:35:22 -0400

    App files

---
+++ app/src/main/java/com/ajs/bloknot/Task.java
@@ -0,0 +1,18 @@
+package com.ajs.bloknot;
+
+import java.util.Date;
+
+public class Task {
+
+    public String name;
+    public String additionalInfo;
+    public Date date;
+
+    public Task(String name, String additionalInfo, Date date) {
+        this.name = name;
+        this.additionalInfo = additionalInfo;
+        this.date = date;
+    }
+
+
+}
